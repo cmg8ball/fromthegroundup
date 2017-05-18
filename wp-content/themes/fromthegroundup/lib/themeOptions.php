abfdca27820053069ddc30d4e9245a21d1ce9559
cmgLaptop <colin@atomicsmash.co.uk>
2017-05-18 21:21:52 +0100

    Updated login logo image.

--- wp-content/themes/fromthegroundup/lib/themeOptions.php
+++ wp-content/themes/fromthegroundup/lib/themeOptions.php
@@ -2,7 +2,7 @@
 
 function custom_login_logo() {
 
-echo "<style type='text/css'> .login h1 a { background-size: 250px 90px; width: 250px; height: 90px; background-image: url('" . get_bloginfo('template_directory') . "/assets/img/login-logo.jpg') !important;</style>";
+echo "<style type='text/css'> .login h1 a { background-size: 250px 90px; width: 250px; height: 90px; background-image: url('" . get_bloginfo('template_directory') . "/assets/img/login-logo.png') !important;</style>";
 
 }
 
